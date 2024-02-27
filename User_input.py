4a4f4db2698b5ae7273950419a0e097cf2de9654
BensonWathigo <107104031+BensonWathigo@users.noreply.github.com>
2024-02-27 16:05:00 +0300

    Create User_input.py

---
+++ User_input.py
@@ -0,0 +1,13 @@
+# user_input.py
+
+# Get user input for their name
+name = input("Enter your name: WATHIGO ")
+
+# Get user input for their age
+age = input("Enter your age: 22 ")
+
+# Get user input for their location
+location = input("Enter your location: Kasarani ")
+
+# Display a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
